9499bb63a94c0bc5aa6a7262bc7f7b1b0a572687
Jacob L. <lecoqjacob@gmail.com>
2024-03-11 10:05:45 -0500

    chore: update orora-cli
    - remove tmux
    - cleanup

--- sh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-TOOLBX="orora-cli"
-if "${TOOLBX}" ~= "wolfi"; then
-  TOOLBX="wolfi-toolbox"
-fi
-
-echo "./toolboxes/${TOOLBX}/Containerfile"
--- toolboxes/orora-cli/programs.sh
+++ toolboxes/orora-cli/programs.sh
@@ -3,12 +3,12 @@
 set -ouex pipefail
 
 # Rust
-if [ ! -d "$HOME/.cargo" ]; then
-	curl https://sh.rustup.rs -sSf | sh -s -- -y
-fi
+# if [ ! -d "$HOME/.cargo" ]; then
+# 	curl https://sh.rustup.rs -sSf | sh -s -- -y
+# fi
 
 # Tmux
-# mv /tmux /XDG_DIRS/config/tmux &&
+# mv /tmux $HOME/.config/tmux &&
 # 	source /etc/profile.d/01-orora-xdg.sh &&
-# 	git clone https://github.com/tmux-plugins/tpm /XDG_D4IRS/config/tmux/plugins/tpm &&
-# 	/XDG_DIRS/config/tmux/plugins/tpm/bin/install_plugins
+# 	git clone https://github.com/tmux-plugins/tpm $HOME/.config/tmux/plugins/tpm &&
+# 	$HOME/.config/tmux/plugins/tpm/bin/install_plugins
